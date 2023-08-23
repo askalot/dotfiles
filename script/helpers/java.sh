52be9b582e753e3db0f1ed274ae4f90d3af16a79
Arunan Skanthan <59402100+askalot@users.noreply.github.com>
2023-08-23 23:06:17 +1000

    Fix typo in Java config

--- script/helpers/java.sh
+++ script/helpers/java.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 setup_java() {
-	asdf plugin add java https://github.com/asdf-vm/halcyon/asdf-java.git
+	asdf plugin add java https://github.com/halcyon/asdf-java.git
 
 	asdf install java "temurin-17.0.8+7"
 
