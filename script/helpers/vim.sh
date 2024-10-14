2371e07adc094d899f0d8ea527701b9e92b224aa
Arunan Skanthan <59402100+askalot@users.noreply.github.com>
2024-10-14 22:03:59 +1100

    Update Vim setup script to use GNU Stow

--- script/helpers/vim.sh
+++ script/helpers/vim.sh
@@ -2,11 +2,11 @@
 
 setup_vim() {
 	# Install vim-plug
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	curl -fLo "$HOME/.config/vim/autoload/plug.vim" --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-	# Copy config template to config directory
-	cp "../vim/.vimrc" "$HOME/.vimrc"
+	# Link Vim config
+	stow vim --target="$HOME" --dir "../"
 
 	# Install Vim plugins
 	vim +'PlugInstall --sync' +qa
