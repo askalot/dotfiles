75cc11dbeb3956b2e6860e849a4035c2587d600c
Arunan Skanthan <59402100+askalot@users.noreply.github.com>
2024-11-14 12:58:11 +1100

    Update eza config

--- script/helpers/eza.sh
+++ script/helpers/eza.sh
@@ -1,8 +1,9 @@
 setup_eza() {
 	LINE_1='alias la="eza --all --group-directories-first --icons --long"'
 	LINE_2='alias lt="eza --all --group-directories-first --icons --tree --git-ignore"'
+	LINE_3='alias ll="eza --all --group-directories-first --icons --long --no-permissions --no-filesize --no-user --no-time --git"'
 
-	for string in "$LINE_1" "$LINE_2"; do
+	for string in "$LINE_1" "$LINE_2" "$LINE_3"; do
 			echo "$string" >> "$HOME/.zshrc"
 	done
 }
